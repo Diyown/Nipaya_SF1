4ad6234cbf57d8119da79e0ba357daf3a0b9b39f
Dione <louisnipaya2@gmail.com>
2023-04-19 23:21:57 +0800

    Nipaya_SF2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        internal object textBox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.GetItemText("Selected Item: " + comboBox1.SelectedItem);
+            comboBox1.Items.Insert(0, textBox1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+            label1.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Sorted = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           //addButton newform = new addButton();
+           // newform.ShowDialog();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Remove(comboBox1.SelectedIndex);
+            comboBox1.Text = string.Empty;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class addButton : Form
+    {
+        private Form1 form1;
+        public addButton(Form1 form1)
+        {
+            InitializeComponent();
+
+            Form1 frm = new Form1();
+            this.form1 = form1;
+        }
+
+        private void addButton_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+} 
+
